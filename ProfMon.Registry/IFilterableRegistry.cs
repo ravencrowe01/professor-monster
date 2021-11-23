131b0bb27513d26ddcb3f747865f8ed33c7e338b
ravencrowe01 <ravencrowe01@gmail.com>
2021-11-23 18:25:05 -0500

    Removed DataLoading and Registry, as they are outside of the scope of this project

--- ProfMon.Registry/IFilterableRegistry.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProfMon.Base.ProfObj;
-using ProfMon.Registry.Filter;
-using System.Collections.Generic;
-
-namespace ProfMon.Registry {
-    public interface IFilterableRegistry<T> : IReadOnlyRegistry<T> where T : BaseProfObj {
-        IEnumerable<T> Filter (IBaseFilter registryFilter);
-    }
-}
